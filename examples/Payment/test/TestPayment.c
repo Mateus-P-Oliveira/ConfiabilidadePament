691bbb8dc5fd9770aae6eec744a18a5d72bc03d9
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2024-09-08 16:17:05 +0000

    Feita as funções de teste

--- examples/Payment/test/TestPayment.c
+++ examples/Payment/test/TestPayment.c
@@ -13,7 +13,217 @@ TEST_TEAR_DOWN(Payment)
 {
 }
 
-TEST(Payment, Testpayment1)
+TEST(Payment, Testpayment1) {
+    float pagamento = 0.01;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment2) {
+    float pagamento = 9999.99;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment3) {
+    float pagamento = 10000.00;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment4) {
+    float pagamento = 19999.99;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment5) {
+    float pagamento = 20000.00;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment6) {
+    float pagamento = 99999.00;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment7) {
+    float pagamento = 0.01;
+    char tipo[15] = "estudante";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment8) {
+    float pagamento = 9999.99;
+    char tipo[15] = "estudante";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment9) {
+    float pagamento = 10000.00;
+    char tipo[15] = "estudante";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment10) {
+    float pagamento = 19999.99;
+    char tipo[15] = "estudante";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment11) {
+    float pagamento = 20000.00;
+    char tipo[15] = "estudante";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment12) {
+    float pagamento = 99999.00;
+    char tipo[15] = "estudante";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment13) {
+    float pagamento = 0.01;
+    char tipo[15] = "aposentado";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment14) {
+    float pagamento = 9999.99;
+    char tipo[15] = "aposentado";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment15) {
+    float pagamento = 10000.00;
+    char tipo[15] = "aposentado";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment16) {
+    float pagamento = 19999.99;
+    char tipo[15] = "aposentado";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment17) {
+    float pagamento = 20000.00;
+    char tipo[15] = "aposentado";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment18) {
+    float pagamento = 99999.00;
+    char tipo[15] = "aposentado";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment19) {
+    float pagamento = 0.01;
+    char tipo[15] = "VIP";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment20) {
+    float pagamento = 9999.99;
+    char tipo[15] = "VIP";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment21) {
+    float pagamento = 10000.00;
+    char tipo[15] = "VIP";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment22) {
+    float pagamento = 19999.99;
+    char tipo[15] = "VIP";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment23) {
+    float pagamento = 20000.00;
+    char tipo[15] = "VIP";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment24) {
+    float pagamento = 99999.00;
+    char tipo[15] = "VIP";
+    TEST_ASSERT_EQUAL(0, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment25) {
+    float pagamento = 0.00;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(1, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment26) {
+    float pagamento = 100000.00;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(1, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment27) {
+    float pagamento = -1.00;
+    char tipo[15] = "regular";
+    TEST_ASSERT_EQUAL(1, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment28) {
+    float pagamento = 0.01;
+    char tipo[15] = "invalido1";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment29) {
+    float pagamento = 9999.99;
+    char tipo[15] = "invalido2";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment30) {
+    float pagamento = 10000.00;
+    char tipo[15] = "invalido3";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment31) {
+    float pagamento = 19999.99;
+    char tipo[15] = "invalido1";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment32) {
+    float pagamento = 20000.00;
+    char tipo[15] = "invalido2";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment33) {
+    float pagamento = 99999.00;
+    char tipo[15] = "invalido3";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment34) {
+    float pagamento = 0.01;
+    char tipo[15] = "invalido4";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment35) {
+    float pagamento = 99999.00;
+    char tipo[15] = "invalido4";
+    TEST_ASSERT_EQUAL(2, payment(pagamento, tipo));
+}
+
+TEST(Payment, Testpayment36)
 {
   // All of these should pass
   float pagamento = 730.11;
@@ -22,9 +232,3 @@ TEST(Payment, Testpayment1)
   TEST_ASSERT_EQUAL(0, payment(pagamento,tipo));
   //TEST_ASSERT_EQUAL(-1, payment(2,2));
 }
-
-/*TEST(payment, Testpayment2)
-{
-  // This test will fail
-  TEST_ASSERT_EQUAL(2, payment(1,2));
-}*/
--- examples/Payment/test/test_runners/TestPayment_Runner.c
+++ examples/Payment/test/test_runners/TestPayment_Runner.c
@@ -4,5 +4,39 @@
 TEST_GROUP_RUNNER(Payment)
 {
   RUN_TEST_CASE(Payment, Testpayment1);
-  //RUN_TEST_CASE(payment, Testpayment2);
+  RUN_TEST_CASE(Payment, Testpayment2);
+  RUN_TEST_CASE(Payment, Testpayment3);
+  RUN_TEST_CASE(Payment, Testpayment4);
+  RUN_TEST_CASE(Payment, Testpayment5);
+  RUN_TEST_CASE(Payment, Testpayment6);
+  RUN_TEST_CASE(Payment, Testpayment7);
+  RUN_TEST_CASE(Payment, Testpayment8);
+  RUN_TEST_CASE(Payment, Testpayment9);
+  RUN_TEST_CASE(Payment, Testpayment10);
+  RUN_TEST_CASE(Payment, Testpayment11);
+  RUN_TEST_CASE(Payment, Testpayment12);
+  RUN_TEST_CASE(Payment, Testpayment13);
+  RUN_TEST_CASE(Payment, Testpayment14);
+  RUN_TEST_CASE(Payment, Testpayment15);
+  RUN_TEST_CASE(Payment, Testpayment16);
+  RUN_TEST_CASE(Payment, Testpayment17);
+  RUN_TEST_CASE(Payment, Testpayment18);
+  RUN_TEST_CASE(Payment, Testpayment19);
+  RUN_TEST_CASE(Payment, Testpayment20);
+  RUN_TEST_CASE(Payment, Testpayment21);
+  RUN_TEST_CASE(Payment, Testpayment22);
+  RUN_TEST_CASE(Payment, Testpayment23);
+  RUN_TEST_CASE(Payment, Testpayment24);
+  RUN_TEST_CASE(Payment, Testpayment25);
+  RUN_TEST_CASE(Payment, Testpayment26);
+  RUN_TEST_CASE(Payment, Testpayment27);
+  RUN_TEST_CASE(Payment, Testpayment28);
+  RUN_TEST_CASE(Payment, Testpayment29);
+  RUN_TEST_CASE(Payment, Testpayment30);
+  RUN_TEST_CASE(Payment, Testpayment31);
+  RUN_TEST_CASE(Payment, Testpayment32);
+  RUN_TEST_CASE(Payment, Testpayment33);
+  RUN_TEST_CASE(Payment, Testpayment34);
+  RUN_TEST_CASE(Payment, Testpayment35);
+  RUN_TEST_CASE(Payment, Testpayment36);
 }
